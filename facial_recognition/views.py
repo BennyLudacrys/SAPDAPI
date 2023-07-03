3cf4c45c6535109f189c131dd0b04ff7910611f3
benny ludacrys <bennedito01@gmail.com>
2023-07-03 19:53:45 +0200

    implementation of face recognition using image uploaded

--- facial_recognition/views.py
+++ facial_recognition/views.py
@@ -1,3 +1,9 @@
+import face_recognition
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.files.storage import FileSystemStorage
+from PIL import Image, ImageDraw
 import cloudinary
 from rest_framework.decorators import api_view
 from cloudinary_storage.storage import RawMediaCloudinaryStorage
@@ -13,11 +19,15 @@ from posts.models import Post
 
 @api_view(['POST'])
 def detect_image(request):
-    if 'image' not in request.FILES:
-        return Response({'error': 'Nenhuma imagem fornecida.'}, status=400)
+    # upload image
+    if request.method == 'POST' and request.FILES.get('image'):
+        myfile = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        # person = Person.objects.create(name="Swimoz", user_id="1", address="2020 Nehosho", picture=uploaded_file_url[1:])
+        # person.save()
 
-    # Upload image
-    storage = RawMediaCloudinaryStorage()
     images = []
     encodings = []
     names = []
@@ -27,47 +37,60 @@ def detect_image(request):
     for post in posts:
         images.append(post.first_name + '_image')
         encodings.append(post.first_name + '_face_encoding')
-        files.append(post.picture.name)
+        files.append(post.picture)
         names.append(post.first_name + ' ' + post.address)
 
     for i in range(0, len(images)):
-        file_obj = storage.open(files[i])
-        images[i] = face_recognition.load_image_file(file_obj)
+        images[i] = face_recognition.load_image_file(files[i])
         encodings[i] = face_recognition.face_encodings(images[i])[0]
 
+    # Create arrays of known face encodings and their names
     known_face_encodings = encodings
     known_face_names = names
 
-    image = request.FILES['image']
-    uploaded_file_name = default_storage.save(image.name, ContentFile(image.read()))
-    uploaded_file_url = default_storage.url(uploaded_file_name)
-
+    # Load an image with an unknown face
     unknown_image = face_recognition.load_image_file(uploaded_file_url[1:])
+
+    # Find all the faces and face encodings in the unknown image
     face_locations = face_recognition.face_locations(unknown_image)
     face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
 
+    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+    # See http://pillow.readthedocs.io/ for more about PIL/Pillow
     pil_image = Image.fromarray(unknown_image)
+    # Create a Pillow ImageDraw Draw instance to draw with
     draw = ImageDraw.Draw(pil_image)
 
+    # Loop through each face found in the unknown image
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Desconhecido"
 
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
 
+        # Draw a box around the face using the Pillow module
         draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+        # Draw a label with a name below the face
         text_width, text_height = draw.textsize(name)
         draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
         draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
 
+    # Remove the drawing library from memory as per the Pillow docs
     del draw
 
-    # Salvar a imagem resultante
-    result_image_name = f"image_with_boxes_{uploaded_file_name}"
-    result_image_path = f"media/{result_image_name}"
-    pil_image.save(result_image_path)
+    # Save the resulting image to disk
+    pil_image.save(fs.path(filename))
 
-    return Response({'result_image': result_image_name})
+    return Response(status=status.HTTP_200_OK)
